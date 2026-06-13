21cba5fa89842f35205bcc61c2d63d3f763da63d
code-with-ricky <amrikdevofficial@gmail.com>
2026-06-13 16:27:20 +0530

    first app

---
+++ ch-1-first-app/src/App.jsx
@@ -0,0 +1,18 @@
+const App = () => {
+  // return (
+  //   // can return only one thing as function can return one item
+  //   <h1>Hello World</h1>
+  //   // <h2>Hello World</h2> // this will give error
+  // )
+
+  // solution, wrap both inside div
+  // or wrap inside fragment
+  return (
+    <>
+      <h1>Hello World</h1>
+      <h2>Hello World</h2>
+    </>
+  );
+};
+
+export default App;
